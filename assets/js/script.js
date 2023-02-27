488763b2de8558eb68ad5f47336997e63363bd6b
michael-hoang <mike081522@gmail.com>
2023-02-26 16:41:56 -0800

    Create playRound function

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,47 @@ function getComputerChoice() {
             return "Scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection_f = playerSelection.toLowerCase()
+    if (playerSelection_f === "rock") {
+        if (computerSelection === "Rock") {
+            return "It's a draw!";
+        }
+        else if (computerSelection === "Paper") {
+            return "You lose! Paper beats Rock";
+        }
+        else {
+            return "You win! Rock beats Scissors"
+        }
+    }
+    else if (playerSelection_f === "paper") {
+        if (computerSelection === "Rock") {
+            return "You win! Paper beats Rock"
+        }
+        else if (computerSelection == "Paper") {
+            return "It's a draw!"
+        }
+        else {
+            return "You lose! Scissors beat Paper"
+        }
+    }
+    else if (playerSelection_f === "scissors") {
+        if (computerSelection == "Rock") {
+            return "You lose! Rock beats Scissors"
+        }
+        else if (computerSelection == "Paper") {
+            return "You win! Scissors beat Paper"
+        }
+        else {
+            return "It's a draw!"
+        }
+    }
+    else{
+        return "Please use only Rock, Paper, or Scissors."
+    }
+}
+
+const playerSelection = "papeR";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
